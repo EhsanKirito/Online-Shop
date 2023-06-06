fb07eb6cd1498d1d69ed8f5244a1812a5f0fd496
Ehsan <ehsanasadimoghadam@gamil.com>
2023-06-06 16:09:08 +0330

    add repository

---
+++ app/src/main/java/com/example/onlineshop/data/Repository/ShopRepository.kt
@@ -0,0 +1,17 @@
+package com.example.onlineshop.data.Repository
+
+import com.example.onlineshop.data.network.model.dto.product.Product
+import com.example.onlineshop.data.network.model.ui.ProductItem
+import com.example.onlineshop.data.network.remotedatasource.ShopRemoteDataSource
+import com.example.onlineshop.data.network.safeapicall.ResponseState
+import com.example.onlineshop.data.network.safeapicall.asResponseState
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ShopRepository @Inject constructor(
+    private val shopRemoteDataSource: ShopRemoteDataSource
+) {
+    fun getProducts(page:Int): Flow<ResponseState<List<ProductItem>>> =shopRemoteDataSource.getProducts(page).asResponseState()
+
+    fun getProduct(id:Int): Flow<ResponseState<List<ProductItem>>> =shopRemoteDataSource.getProducts(id).asResponseState()
+}
