4a9dcbbb7b7e71426e8e5069f0529fb9d6dbf50a
Ehsan <ehsanasadimoghadam@gamil.com>
2023-06-07 22:43:55 +0330

    add recycler view setups

--- app/src/main/java/com/example/onlineshop/ui/features/home/HomeFragment.kt
+++ app/src/main/java/com/example/onlineshop/ui/features/home/HomeFragment.kt
@@ -7,16 +7,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.example.onlineshop.R
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.example.onlineshop.databinding.FragmentHomeBinding
+import com.example.onlineshop.ui.features.home.adapter.HomeAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
-    private val viewModel : HomeViewModel by viewModels()
     private val binding get() = _binding!!
+    private val viewModel : HomeViewModel by viewModels()
 
+    private lateinit var adapter1: HomeAdapter
+    private lateinit var adapter2: HomeAdapter
+    private lateinit var adapter3: HomeAdapter
+    private lateinit var recyclerView1: RecyclerView
+    private lateinit var recyclerView2: RecyclerView
+    private lateinit var recyclerView3: RecyclerView
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -34,11 +44,52 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        recyclerViewInit1()
+        recyclerViewInit2()
+        recyclerViewInit3()
 
+
+
+
+    }
+
+    fun recyclerViewInit1(){
+        recyclerView1 = binding.rvnewest
+        recyclerView1.layoutManager = LinearLayoutManager(binding.root.context, LinearLayoutManager.HORIZONTAL, true)
+        adapter1 = HomeAdapter {
+            findNavController().navigate(
+                HomeFragmentDirections.actionHomeFragmentToDetailsFragment(it)
+            )
+        }
+        recyclerView1.adapter = adapter1
         viewModel.products.observe(viewLifecycleOwner){
-            Log.e("TAG", "onViewCreated: ${it.size}" )
-            binding.txt.text = it.toString()
-            Log.e("TAG", "${it.toString()}", )
+            adapter1.submitList(it)
+        }
+    }
+    fun recyclerViewInit2(){
+        recyclerView2 = binding.rvmostvisited
+        recyclerView2.layoutManager = LinearLayoutManager(binding.root.context, LinearLayoutManager.HORIZONTAL, true)
+        adapter2 = HomeAdapter {
+            findNavController().navigate(
+                HomeFragmentDirections.actionHomeFragmentToDetailsFragment(it)
+            )
+        }
+        recyclerView2.adapter = adapter2
+        viewModel.products.observe(viewLifecycleOwner){
+            adapter2.submitList(it)
+        }
+    }
+    fun recyclerViewInit3(){
+        recyclerView3 = binding.rvbest
+        recyclerView3.layoutManager = LinearLayoutManager(binding.root.context, LinearLayoutManager.HORIZONTAL, true)
+        adapter3 = HomeAdapter {
+            findNavController().navigate(
+                HomeFragmentDirections.actionHomeFragmentToDetailsFragment(it)
+            )
+        }
+        recyclerView3.adapter = adapter1
+        viewModel.products.observe(viewLifecycleOwner){
+            adapter3.submitList(it)
         }
     }
 
