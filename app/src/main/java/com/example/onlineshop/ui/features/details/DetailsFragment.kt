f7079ef6aba90b395bb8bade915e1e7096d7da86
Ehsan <ehsanasadimoghadam@gamil.com>
2023-06-07 22:40:05 +0330

    add details fragment

---
+++ app/src/main/java/com/example/onlineshop/ui/features/details/DetailsFragment.kt
@@ -0,0 +1,31 @@
+package com.example.onlineshop.ui.features.details
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.onlineshop.databinding.FragmentDetailsBinding
+
+
+class DetailsFragment : Fragment() {
+    private var _binding: FragmentDetailsBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
