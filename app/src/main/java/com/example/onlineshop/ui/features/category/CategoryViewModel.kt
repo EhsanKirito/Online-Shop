67fa3f99449613c8a072f7b7cffed677363264c3
Ehsan <ehsanasadimoghadam@gamil.com>
2023-06-09 10:53:40 +0330

    add category viewmodel

--- app/src/main/java/com/example/onlineshop/ui/features/category/CategoryViewModel.kt
+++ app/src/main/java/com/example/onlineshop/ui/features/category/CategoryViewModel.kt
@@ -3,15 +3,33 @@ package com.example.onlineshop.ui.features.category
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.onlineshop.data.Repository.ShopRepository
+import com.example.onlineshop.data.network.model.ui.CategoryItem
+import com.example.onlineshop.data.network.model.ui.ProductItem
+import com.example.onlineshop.data.network.safeapicall.ResponseState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CategoryViewModel @Inject constructor(private val shopRepository: ShopRepository)  : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    private val _categoryList =
+        MutableStateFlow<ResponseState<List<CategoryItem>>>(ResponseState.Loading)
+    val categoryList: StateFlow<ResponseState<List<CategoryItem>>> = _categoryList
+
+init {
+    getCategoryList()
+}
+
+    private fun getCategoryList() {
+        viewModelScope.launch {
+            shopRepository.getAllCategories().collect { responseState ->
+                _categoryList.emit(responseState)
+            }
+        }
     }
-    val text: LiveData<String> = _text
 }
